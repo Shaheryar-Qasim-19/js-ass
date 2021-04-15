513d5fca1cbc817bb357fc2b9c7a5e9498a35644
Shaheryar-Qasim-19 <shaheryar.qasim@gmail.com>
2021-04-15 10:03:17 +0500

    Question 06

---
+++ app.js
@@ -0,0 +1,144 @@
+// *******************Chapter 05 ****************************
+
+
+// ******************Question # 01 ****************************
+// Addtion
+var a = 5;
+var b = 4;
+var c = a + b;
+document.write("Sum of "+a+" and "+b+" is \n "+c);
+document.write("<br>");
+document.write("<br>");
+
+
+
+// ******************Question # 02 ****************************
+// Subtraction
+var a = 5;
+var b = 4;
+var c = a - b;
+document.write("\nSubtraction of "+a+" and "+b+" is "+c);
+document.write("<br>");
+document.write("<br>");
+
+
+
+// ******************Question # 02 ****************************
+// Multiply
+var a = 5;
+var b = 4;
+var c = a * b;
+document.write("\nMultiplication of "+a+" and "+b+" is "+c);
+document.write("<br>");
+document.write("<br>");
+
+
+
+// ******************Question # 02 ****************************
+// Divide
+var a = 5;
+var b = 4;
+var c = a / b;
+document.write("\nDivision of "+a+" and "+b+" is "+c);
+document.write("<br>");
+document.write("<br>");
+
+
+
+// ******************Question # 02 ****************************
+// Modulus
+var a = 5;
+var b = 4;
+var c = a % b;
+document.write("\nModulus of "+a+" and "+b+" is "+c);
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+// ******************Question # 03 ****************************
+
+// ************ a ***************
+var sherry;
+// ************ b ***************
+document.write("Value after variable declaration is:" + sherry);
+document.write("<br>");
+document.write("<br>");
+
+// ************ c ***************
+sherry = 05;
+
+//d
+document.write("Initial    value: " + sherry);
+document.write("<br>");
+document.write("<br>");
+
+// e
+sherry++
+
+//f
+document.write("value after increment is: " + sherry);
+document.write("<br>");
+document.write("<br>");
+
+
+//g
+// ************** 6  +  7   ****************
+var result = sherry + 7;
+
+
+//h
+alert("Value after addition is " + result);
+
+
+//i
+result--
+//j
+document.write("value after decrement is: " + result);
+document.write("<br>");
+document.write("<br>");
+
+//k 
+var reminder = result % 3;
+
+// L
+document.write("The reminder is: " + reminder);
+document.write("<br>");
+document.write("<br>");
+
+// ******************Question # 04 ****************************
+var tic = 600, buy = 5;
+var cost = tic * buy;
+document.write("Total cost to buy" + buy + "tickets to a movie is " + cost+"PKR");
+document.write("<br>");
+document.write("<br>");
+
+// ******************Question # 05 ****************************
+var number = +prompt("Enter a Number to print a Table:");
+
+document.write(number + " * " + 1 + " = " + number*1);
+document.write("<br>");
+document.write(number + " * " + 2 + " = " + number*2);
+document.write("<br>");
+document.write(number + " * " + 3 + " = " + number*3);
+document.write("<br>");
+document.write(number + " * " + 4 + " = " + number*4);
+document.write("<br>");
+document.write(number + " * " + 5 + " = " + number*5);
+document.write("<br>");
+document.write(number + " * " + 6 + " = " + number*6);
+document.write("<br>");
+document.write(number + " * " + 7 + " = " + number*7);
+document.write("<br>");
+document.write(number + " * " + 8 + " = " + number*8);
+document.write("<br>");
+document.write(number + " * " + 9 + " = " + number*9);
+document.write("<br>");
+document.write(number + " * " + 10 + " = " + number*10);
+document.write("<br>");
+document.write("<br>");
+
+
+// ******************Question # 06 ****************************
+var C = 25, F = 77;
