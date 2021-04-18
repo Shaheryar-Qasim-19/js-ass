c8977154e472f7b759e66f0496376a0531cca5e9
Shaheryar-Qasim-19 <shaheryar.qasim@gmail.com>
2021-04-18 18:01:01 +0500

    chapter 6 to 9 updated

--- app2.js
+++ app2.js
@@ -1,63 +1,62 @@
-// // **********************Chapter 06 to 09**************************
+// **********************Chapter 06 to 09**************************
 
-// // ***************Question 01****************************
-// document.write("sir is question ko check kar len theek kiya hy???<br>")
-// var a = 10;
-// document.write("<br> Result<br> The value of a is: " + a);
-// document.write("<br>.................................<br>")
+// ***************Question 01****************************
+document.write("sir is question ko check kar len theek kiya hy???<br>")
+var a = 10;
+document.write("<br> Result<br> The value of a is: " + a);
+document.write("<br>.................................<br>")
 
-// document.write("<br>The value of ++a is:" + a);
-// document.write("<br>Now the value of a is:" + ++a);
-// document.write("<br><br<br><br>")
+document.write("<br>The value of ++a is:" + a);
+document.write("<br>Now the value of a is:" + ++a);
+document.write("<br><br<br><br>")
 
-// document.write("<br>The value of a++ is:" + a);
-// document.write("<br>Now the value of a is:" + a++);
-// document.write("<br><br><br><br>")
+document.write("<br>The value of a++ is:" + a);
+document.write("<br>Now the value of a is:" + a++);
+document.write("<br><br><br><br>")
 
-// document.write("<br>The value of --a is:" + a);
-// document.write("<br>Now the value of a is:" + --a);
-// document.write("<br><br><br><br>")
+document.write("<br>The value of --a is:" + a);
+document.write("<br>Now the value of a is:" + --a);
+document.write("<br><br><br><br>")
 
-// document.write("<br>The value of a-- is:" + a);
-// document.write("<br>Now the value of a is:" + a--);
-// document.write("<br><br><br><br><hr>")
+document.write("<br>The value of a-- is:" + a);
+document.write("<br>Now the value of a is:" + a--);
+document.write("<br><br><br><br><hr>")
 
-// // Sir is question ko check kar len upar walay ko
-
-
-
-// // ***************Question 02************************
-// var a = 2, b = 1;
-// var result = --a - --b + ++b + b--;
-// //            1  -  0  +  1  +  1  
-// document.write("<br>a is : " + a + "<br> b is : " + b + "<br> result is : " + result);
-
-// // ***************Question 03************************
-// var name = prompt("Enter your name here!" );
-// alert("Hello! " + name + " nice to meet you");
-// document.write("<br><br><hr>");
+// Sir is question ko check kar len upar walay ko
 
 
 
-// // ***************Question 05************************
-// var Num = +prompt("Enter a Number to print a Table:");
-// var five = 5;
-// if(Num === +prompt())
-// {
-//     for(var a = 1; a<=10 ; a++){
-//     document.write(Num + " * " + a + " = " + Num*a);
-//     document.write("<br>");
-//     }
-// }
-// else
-// {
-//     for(var a = 1; a<=10 ; a++){
-//         document.write(five + " * " + a + " = " + five*a);
-//         document.write("<br>");
-//         }
-// }
+// ***************Question 02************************
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+//            1  -  0  +  1  +  1  
+document.write("<br>a is : " + a + "<br> b is : " + b + "<br> result is : " + result);
 
-//comment iske uppar hy
+// ***************Question 03************************
+var name = prompt("Enter your name here!" );
+alert("Hello! " + name + " nice to meet you");
+document.write("<br><br><hr>");
+
+
+
+// ***************Question 05************************
+var Num = +prompt("Enter a Number to print a Table:");
+var five = 5;
+if(Num === +prompt())
+{
+    for(var a = 1; a<=10 ; a++){
+    document.write(Num + " * " + a + " = " + Num*a);
+    document.write("<br>");
+    }
+}
+else
+{
+    for(var a = 1; a<=10 ; a++){
+        document.write(five + " * " + a + " = " + five*a);
+        document.write("<br>");
+        }
+}
+
 
 
 // ***************Question 06************************
