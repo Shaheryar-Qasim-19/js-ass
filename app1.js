4be4e32836d520f53c925be52923b07bf6769eb7
Shaheryar-Qasim-19 <shaheryar.qasim@gmail.com>
2021-04-18 16:34:06 +0500

    Chapter 04 complete

---
+++ app1.js
@@ -0,0 +1,24 @@
+// ************************Chapter 04***************************
+
+// *********************Question 01*****************************
+var a, b, c;
+
+// *********************Question 02*****************************
+// Legal Variable
+var num, sum, rum, tum, gum;
+
+// *********************Question 02*****************************
+// Illigal Variable
+// var s a, s%q , a/s, 1f, han@; 
+
+// *********************Question 03*****************************
+// a
+document.write("<h1>Rules for naming JS variables</h1>");
+// b
+document.write("<br>Variable names can only contain numbers, $ and - . For example :$my_1stVariable");
+// c
+document.write("<br>Variables must begin with a letter, $ or _ . For example $name, _name");
+// d
+document.write("<br>Variable name are case sensitive");
+// e
+document.write("<br>Variable name should not be JS keywords<br><hr>");
