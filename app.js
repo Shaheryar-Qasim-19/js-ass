ef05bcd9cb56ac000df9780384db7160ea847ac9
Shaheryar-Qasim-19 <shaheryar.qasim@gmail.com>
2021-04-18 13:25:30 +0500

    chapter 5 completed!

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var a = 5;
 var b = 4;
 var c = a + b;
 document.write("Sum of "+a+" and "+b+" is \n "+c);
-document.write("<br>");
+document.write("<hr>");
 document.write("<br>");
 
 
@@ -18,7 +18,7 @@ var a = 5;
 var b = 4;
 var c = a - b;
 document.write("\nSubtraction of "+a+" and "+b+" is "+c);
-document.write("<br>");
+document.write("<hr>");
 document.write("<br>");
 
 
@@ -29,7 +29,7 @@ var a = 5;
 var b = 4;
 var c = a * b;
 document.write("\nMultiplication of "+a+" and "+b+" is "+c);
-document.write("<br>");
+document.write("<hr>");
 document.write("<br>");
 
 
@@ -40,7 +40,7 @@ var a = 5;
 var b = 4;
 var c = a / b;
 document.write("\nDivision of "+a+" and "+b+" is "+c);
-document.write("<br>");
+document.write("<hr>");
 document.write("<br>");
 
 
@@ -51,7 +51,7 @@ var a = 5;
 var b = 4;
 var c = a % b;
 document.write("\nModulus of "+a+" and "+b+" is "+c);
-document.write("<br>");
+document.write("<hr>");
 document.write("<br>");
 
 
@@ -104,14 +104,14 @@ var reminder = result % 3;
 
 // L
 document.write("The reminder is: " + reminder);
-document.write("<br>");
+document.write("<hr>");
 document.write("<br>");
 
 // ******************Question # 04 ****************************
 var tic = 600, buy = 5;
 var cost = tic * buy;
 document.write("Total cost to buy" + buy + "tickets to a movie is " + cost+"PKR");
-document.write("<br>");
+document.write("<hr>");
 document.write("<br>");
 
 // ******************Question # 05 ****************************
@@ -136,7 +136,7 @@ document.write("<br>");
 document.write(number + " * " + 9 + " = " + number*9);
 document.write("<br>");
 document.write(number + " * " + 10 + " = " + number*10);
-document.write("<br>");
+document.write("<hr>");
 document.write("<br>");
 
 
@@ -151,6 +151,7 @@ document.write("Celcius = " + F + "Fo <br>" );
 //d
 F = (C * 9/5) +32;
 document.write("Faranheit = " + C + "Co");
+document.write("<hr>");
 
 
 // ******************Question # 07 ****************************
@@ -179,6 +180,7 @@ document.write("<br> Quantity of item 2 is " + quantity2);
 document.write("<br> Shipping Charges is " + shippingcharges);
 var resultant = (item1 * quantity1) + (item2 * quantity2) + shippingcharges;
 document.write("<br> The Cost of your order is " + resultant);
+document.write("<hr>");
 
 
 // ******************Question # 08 ****************************
@@ -188,6 +190,7 @@ document.write("<h1> Mark Sheet </h1> <br> <br>");
 document.write("<br>Total Marks : " + tm);
 document.write("<br>Obtained Marks : " + om);
 document.write("<br>Percentage : " + per + "%");
+document.write("<hr>");
 
 // ******************Question # 09 ****************************
 var dollars = 10, riyals = 25 , pk = 104.8, pks = 28;
@@ -195,11 +198,51 @@ document.write("<h1> Currency in PKR </h1> <br> <br>");
 var conversion = (dollars*pk)+(riyals*pks);
 document.write(conversion);
 document.write("<br>");
+document.write("<hr>");
 
 
 // ******************Question # 10 ****************************
 var num = 2;
 var equation = (((num + 5)*10)/2);
+document.write("Question No: 10<br>");
 document.write(equation);
 document.write("<br>");
+document.write("<hr>");
+
+// ******************Question # 11 ****************************
+document.write("<h1> Age Calculator </h1> <br> <br>");
+var crryear = 2021;
+var birthyear = 2000;
+var age = crryear - birthyear;
+document.write("Current Year: " + crryear + "<br> Birth Year: " + birthyear + "<br>Your Age is: " + age); 
+document.write("<hr>");
+
+
+// ******************Question # 12 ****************************
+document.write("<h1> The Geometrizer </h1> <br> <br>");
+var radius = 20, pie = 3.142, circum, area;
+circum = 2 * pie * radius;
+area = (pie * (radius * radius));
+document.write("<br>Radius of a Circle: " + radius);
+document.write("<br>The Circumference is: " + circum);
+document.write("<br>The area is: " + area);
+document.write("<hr>");
+
+
+// ******************Question # 13 ****************************
+document.write("<h1> The Lifetime Supply Calculator </h1> <br> <br>");
+var favSnack = "Chocolate Chip", currAge = 15, maxAge = 65, esstAmount = 3, ripOld ;
+ripOld = (maxAge - currAge) * esstAmount;
+document.write("<br>Favourite Snack: " + favSnack);
+document.write("<br>Current Age: " + currAge);
+document.write("<br>Estimated maximum Age: " + maxAge);
+document.write("<br>Amount of snacks per day: " + esstAmount);
+document.write("<br>You will need " + ripOld +" "  +  favSnack +" to last you until the rip old  age of " + maxAge);
+document.write("<hr>");
+
+
+
+
+
+
 
