575faf449743ddf3ce9da8f8d80e90a378fcd6a4
Shaheryar-Qasim-19 <shaheryar.qasim@gmail.com>
2021-04-20 11:41:05 +0500

    9 to 11 start

---
+++ chapter09to11.js
@@ -0,0 +1,58 @@
+// Question 01***********************************************************************
+
+// var city = prompt("Enter a city name");
+
+// if(city==="karachi"||city==="Karachi"||city==="KARACHI")
+// {
+//     alert("Welcome to the city of lights");
+// }
+// else
+// {
+//     alert("Enter invalid city");
+// }
+
+
+// Question 02***********************************************************************
+
+// var gender = prompt("Enter your gender, male or female");
+
+// if(gender==="male"||gender==="Male")
+// {
+//     alert("Good Morning Sir");
+// }
+
+// else if(gender==="female"||gender==="female")
+// {
+//     alert("Good Morning Ma'am");
+// }
+// else{
+//     alert("Hello meethey");
+// }
+
+
+// Question 03***********************************************************************
+
+
+// var colorOfRoad = prompt("Enter traffic signal colors");
+
+// // Table me kese karna hai print samajh nhi aya
+
+// if(colorOfRoad==="Red")
+// {
+//     alert("Must Stop");
+// }
+
+
+// else if(colorOfRoad==="Yellow")
+// {
+//     alert("Ready to move");
+// }
+
+
+// else if(colorOfRoad==="Green")
+// {
+//     alert("Move Now");
+// }
+// else{
+//     alert("Entered wrong color");
+// }
