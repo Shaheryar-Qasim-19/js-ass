6bbb85e9fcc02abc8595b3945bdd54a00072ff6f
Shaheryar-Qasim-19 <shaheryar.qasim@gmail.com>
2021-04-24 06:22:20 +0500

    chapter 12-13 completed

---
+++ chapter12-13.js
@@ -0,0 +1,9 @@
+// Question 01*********************************************************************************************
+var input = prompt("Enter a key to check it is uppercase letter, lowercase letter or number");
+
+if(input>=65 && input<=90){
+    document.write("<br> UPPERCASE LETTER");
+}
+else if(input>=97 && input<=122){
+    document.write("<br> lowercase letter");
+}
